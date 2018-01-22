6a37b1b3897dc907173c02cb3252e751adf4f3bd
Avinash Ranjan <avinash.ranjan21@gmail.com>
2018-01-23 00:30:34 +0530

    push notification added and device id received

--- app/src/main/java/com/bugfreetechnology/firebasesample/MainActivity.java
+++ app/src/main/java/com/bugfreetechnology/firebasesample/MainActivity.java
@@ -3,11 +3,16 @@ package com.bugfreetechnology.firebasesample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.iid.FirebaseInstanceId;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+        FirebaseInstanceId.getInstance().getToken();
     }
 }
---
+++ app/src/main/java/com/bugfreetechnology/firebasesample/push_notification/MyFirebaseMessagingService.java
@@ -0,0 +1,8 @@
+package com.bugfreetechnology.firebasesample.push_notification;
+
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+}
